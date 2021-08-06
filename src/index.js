5b5a5b114fad2ad4d3cff871304e5e9eacd89e6e
Yuliya <yuliya.rostova@gmail.com>
2021-08-06 23:36:55 +0300

    feat: add task functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+
+  let min = array[0];
+  for (let i = 1; i <= array.length; i++) {
+    if (min > array[i]) min = array[i];
+  } 
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+
+  let max = array[0];
+  for (let i = 1; i <= array.length; i++) {
+    if (max < array[i]) max = array[i];
+  } 
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    //console.log('SUM: ' + sum + " - ARR[i]: " + array[i]);
+    sum = sum + array[i];
+  } 
+
+  console.log('SUM: ' + sum);
+
+  return sum / (array.length);
 }
